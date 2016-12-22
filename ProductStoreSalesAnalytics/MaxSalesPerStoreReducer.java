b2a791b7000a493013ba2ef4ef1dca99f09f60a6
PARITOSH HIRA <parit2015@users.noreply.github.com>
2016-12-21 19:36:59 -0800

    Create MaxSalesPerStoreReducer.java

---
+++ ProductStoreSalesAnalytics/MaxSalesPerStoreReducer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class MaxSalesPerStoreReducer extends Reducer<Text, DoubleWritable, Text, Text>{
+
+	@Override
+	public void reduce(Text storeName, Iterable<DoubleWritable> salesPerStoreListByMapper, Context context)
+			throws IOException, InterruptedException {
+		
+		Double maxSalesPerStore = 0.0;
+		for (DoubleWritable salesIndividual : salesPerStoreListByMapper) {
+			
+			/*
+			 * Finding out maximum sales value from the salesListFromMapper
+			 */
+			maxSalesPerStore = Math.max(maxSalesPerStore, salesIndividual.get());
+		}
+		
+		/*
+		 * Writing the context for storeName and MaxSales of same store
+		 */
+		context.write(storeName, new Text(new DecimalFormat("#.##").format(maxSalesPerStore)));
+	}
+}

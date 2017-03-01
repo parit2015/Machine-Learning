6aed5fa1aeb92b2dfb28eb7e78eef4c4459117bc
Paritosh "Parit <parit2015@users.noreply.github.com>
2017-03-01 22:08:23 +0530

    Update MusicShopReducer.java

--- musicShop/MusicShopReducer.java
+++ musicShop/MusicShopReducer.java
@@ -1,13 +1,10 @@
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-
-public class MusicShopReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>{
-	
+public class MusicShopReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>{	
 	@Override
 	public void reduce(IntWritable trackId, Iterable<IntWritable> userIds, Context context)
 			throws IOException, InterruptedException{
